bd367d3b0ec687136149acfcd45d19bde5c38250
aria <aryashahbazi@gmail.com>
2021-04-03 13:32:39 +0430

    Slote machine

---
+++ Build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: G:/C++/AW/Slote machine/Slote-machine
+# Build directory: G:/C++/AW/Slote machine/Slote-machine/Build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("tests")
